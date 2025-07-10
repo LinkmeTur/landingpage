e150ca2c9aac0ae3f8301cd20ba44516f208c147
Arthur loeblein <arthurloeblein2006@gmail.com>
2025-07-10 17:12:45 -0300

    Atualiza landing page completa com IDs, estilos e novos assets

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,6 +4,6 @@ export function middleware(req: NextRequest) {
     const url = req.nextUrl;
 
     if (url.hostname === 'linkmetur.com.br' && url.pathname === '/') {
-        return NextResponse.rewrite(new URL('/pages/landingPage', req.url));
+        return NextResponse.rewrite(new URL('/site', req.url));
     }
 }
